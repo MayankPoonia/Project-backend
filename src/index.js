a51bc91b3994f44fffcafc50b3f97f8bf950d772
pooniaMayank <pooniamayank8@gmail.com>
2024-12-29 22:14:09 +0530

    path issues resolved

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { fileURLToPath } from "url";
 
 const app = express();
 
-// Get the current directory in ES modules
+// Get the current directory in ES modules..
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
