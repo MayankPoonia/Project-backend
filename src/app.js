4726a091b1fc66b9f6dc49a8390497d63c28024e
Rohit Pahadia <rp3051007@gmail.com>
2025-01-03 18:36:01 +0530

    testing on postman

--- src/app.js
+++ src/app.js
@@ -12,12 +12,12 @@ app.use(cors({
 
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
-app.use(express.status("public"))
+app.use(express.static("public"));
 app.use(cookieParser())
 
 
 //routes import 
-import userRouter from './routes/user.routes'
+import userRouter from './routes/user.routes.js'
 
 
 //routes declaration
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,7 +3,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 //ye method run kab hoga iske liye routes banega jaha pe url hit ho
 const registerUser = asyncHandler( async (req, res) => {
     res.status(200).json({
-        message: "ok" //json reponse dunga ok
+        message: "Ok   " //json reponse dunga ok
     })
 })
 
